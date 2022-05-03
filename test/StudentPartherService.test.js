f027ee9333fc0baa9e8cc5a9f8e979e7fca1979b
cesarmezab <99105052+CesarMezaB@users.noreply.github.com>
2022-05-03 15:45:19 -0700

    Add method in StudentPartherService that returns students with certification and their test

--- test/StudentPartherService.test.js
+++ test/StudentPartherService.test.js
@@ -19,4 +19,22 @@ describe("Test para StudentPartherService", () =>{
         const totalStudents = StudentPartherService.filterStudents(studens);
         expect(totalStudents.length).toBe(1)
     })
+
+    test("Requerimiento 2: Regresar todos los estudiantes con certificacion", () =>{
+        const studens = [
+            {
+              "id": "6264d5d89f1df827eb84bb23",
+              "name": "Warren",
+              "email": "Todd@visualpartnership.xyz",
+              "credits": 508,
+              "enrollments": [
+                "Visual Thinking Intermedio",
+                "Visual Thinking Avanzado"
+              ],
+              "previousCourses": 1,
+              "haveCertification": true
+            }]
+        const totalStudents = StudentPartherService.getStudentsCertication(studens);
+        expect(totalStudents[0]).toBe(true)
+    })
 })
